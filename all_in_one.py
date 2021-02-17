87037fef5b37c54e4637f82fdf9145a68ec6d508
ItsArghya <cs15resch11001@iith.ac.in>
2021-02-17 12:58:25 +0530

    Update all_in_one.py

--- all_in_one.py
+++ all_in_one.py
@@ -24,499 +24,59 @@ def make_checkpoint(datei):
   res = ModelCheckpoint(datei, monitor='val_loss', save_best_only = True);
   return(res);
   
+
+ # Essential Definitions
+
+def WORD_SIZE():
+    return(16);
+
+def ALPHA():
+    return(7);
+
+def BETA():
+    return(2);
+
+MASK_VAL = 2 ** WORD_SIZE() - 1;
+
+def shuffle_together(l):
+    state = np.random.get_state();
+    for x in l:
+        np.random.set_state(state);
+        np.random.shuffle(x);
+
+def rol(x,k):
+    return(((x << k) & MASK_VAL) | (x >> (WORD_SIZE() - k)));
+
+def ror(x,k):
+    return((x >> k) | ((x << (WORD_SIZE() - k)) & MASK_VAL));
+
+def enc_one_round(p, k):
+    c0, c1 = p[0], p[1];
+    c0 = ror(c0, ALPHA());
+    c0 = (c0 + c1) & MASK_VAL;
+    c0 = c0 ^ k;
+    c1 = rol(c1, BETA());
+    c1 = c1 ^ c0;
+    return(c0,c1);
+
+def dec_one_round(c,k):
+    c0, c1 = c[0], c[1];
+    c1 = c1 ^ c0;
+    c1 = ror(c1, BETA());
+    c0 = c0 ^ k;
+    c0 = (c0 - c1) & MASK_VAL;
+    c0 = rol(c0, ALPHA());
+    return(c0, c1);
+
+def expand_key(k, t):
+    ks = [0 for i in range(t)];
+    ks[0] = k[len(k)-1];
+    l = list(reversed(k[:len(k)-1]));
+    for i in range(t-1):
+        l[i%3], ks[i+1] = enc_one_round((l[i%3], ks[i]), i);
+    return(ks);# Essential Definitions
   
-  # Essential Definitions
-
-def WORD_SIZE():
-    return(16);
-
-def ALPHA():
-    return(7);
-
-def BETA():
-    return(2);
-
-MASK_VAL = 2 ** WORD_SIZE() - 1;
-
-def shuffle_together(l):
-    state = np.random.get_state();
-    for x in l:
-        np.random.set_state(state);
-        np.random.shuffle(x);
-
-def rol(x,k):
-    return(((x << k) & MASK_VAL) | (x >> (WORD_SIZE() - k)));
-
-def ror(x,k):
-    return((x >> k) | ((x << (WORD_SIZE() - k)) & MASK_VAL));
-
-def enc_one_round(p, k):
-    c0, c1 = p[0], p[1];
-    c0 = ror(c0, ALPHA());
-    c0 = (c0 + c1) & MASK_VAL;
-    c0 = c0 ^ k;
-    c1 = rol(c1, BETA());
-    c1 = c1 ^ c0;
-    return(c0,c1);
-
-def dec_one_round(c,k):
-    c0, c1 = c[0], c[1];
-    c1 = c1 ^ c0;
-    c1 = ror(c1, BETA());
-    c0 = c0 ^ k;
-    c0 = (c0 - c1) & MASK_VAL;
-    c0 = rol(c0, ALPHA());
-    return(c0, c1);
-
-def expand_key(k, t):
-    ks = [0 for i in range(t)];
-    ks[0] = k[len(k)-1];
-    l = list(reversed(k[:len(k)-1]));
-    for i in range(t-1):
-        l[i%3], ks[i+1] = enc_one_round((l[i%3], ks[i]), i);
-    return(ks);# Essential Definitions
-
-def WORD_SIZE():
-    return(16);
-
-def ALPHA():
-    return(7);
-
-def BETA():
-    return(2);
-
-MASK_VAL = 2 ** WORD_SIZE() - 1;
-
-def shuffle_together(l):
-    state = np.random.get_state();
-    for x in l:
-        np.random.set_state(state);
-        np.random.shuffle(x);
-
-def rol(x,k):
-    return(((x << k) & MASK_VAL) | (x >> (WORD_SIZE() - k)));
-
-def ror(x,k):
-    return((x >> k) | ((x << (WORD_SIZE() - k)) & MASK_VAL));
-
-def enc_one_round(p, k):
-    c0, c1 = p[0], p[1];
-    c0 = ror(c0, ALPHA());
-    c0 = (c0 + c1) & MASK_VAL;
-    c0 = c0 ^ k;
-    c1 = rol(c1, BETA());
-    c1 = c1 ^ c0;
-    return(c0,c1);
-
-def dec_one_round(c,k):
-    c0, c1 = c[0], c[1];
-    c1 = c1 ^ c0;
-    c1 = ror(c1, BETA());
-    c0 = c0 ^ k;
-    c0 = (c0 - c1) & MASK_VAL;
-    c0 = rol(c0, ALPHA());
-    return(c0, c1);
-
-def expand_key(k, t):
-    ks = [0 for i in range(t)];
-    ks[0] = k[len(k)-1];
-    l = list(reversed(k[:len(k)-1]));
-    for i in range(t-1):
-        l[i%3], ks[i+1] = enc_one_round((l[i%3], ks[i]), i);
-    return(ks);# Essential Definitions
-
-def WORD_SIZE():
-    return(16);
-
-def ALPHA():
-    return(7);
-
-def BETA():
-    return(2);
-
-MASK_VAL = 2 ** WORD_SIZE() - 1;
-
-def shuffle_together(l):
-    state = np.random.get_state();
-    for x in l:
-        np.random.set_state(state);
-        np.random.shuffle(x);
-
-def rol(x,k):
-    return(((x << k) & MASK_VAL) | (x >> (WORD_SIZE() - k)));
-
-def ror(x,k):
-    return((x >> k) | ((x << (WORD_SIZE() - k)) & MASK_VAL));
-
-def enc_one_round(p, k):
-    c0, c1 = p[0], p[1];
-    c0 = ror(c0, ALPHA());
-    c0 = (c0 + c1) & MASK_VAL;
-    c0 = c0 ^ k;
-    c1 = rol(c1, BETA());
-    c1 = c1 ^ c0;
-    return(c0,c1);
-
-def dec_one_round(c,k):
-    c0, c1 = c[0], c[1];
-    c1 = c1 ^ c0;
-    c1 = ror(c1, BETA());
-    c0 = c0 ^ k;
-    c0 = (c0 - c1) & MASK_VAL;
-    c0 = rol(c0, ALPHA());
-    return(c0, c1);
-
-def expand_key(k, t):
-    ks = [0 for i in range(t)];
-    ks[0] = k[len(k)-1];
-    l = list(reversed(k[:len(k)-1]));
-    for i in range(t-1):
-        l[i%3], ks[i+1] = enc_one_round((l[i%3], ks[i]), i);
-    return(ks);# Essential Definitions
-
-def WORD_SIZE():
-    return(16);
-
-def ALPHA():
-    return(7);
-
-def BETA():
-    return(2);
-
-MASK_VAL = 2 ** WORD_SIZE() - 1;
-
-def shuffle_together(l):
-    state = np.random.get_state();
-    for x in l:
-        np.random.set_state(state);
-        np.random.shuffle(x);
-
-def rol(x,k):
-    return(((x << k) & MASK_VAL) | (x >> (WORD_SIZE() - k)));
-
-def ror(x,k):
-    return((x >> k) | ((x << (WORD_SIZE() - k)) & MASK_VAL));
-
-def enc_one_round(p, k):
-    c0, c1 = p[0], p[1];
-    c0 = ror(c0, ALPHA());
-    c0 = (c0 + c1) & MASK_VAL;
-    c0 = c0 ^ k;
-    c1 = rol(c1, BETA());
-    c1 = c1 ^ c0;
-    return(c0,c1);
-
-def dec_one_round(c,k):
-    c0, c1 = c[0], c[1];
-    c1 = c1 ^ c0;
-    c1 = ror(c1, BETA());
-    c0 = c0 ^ k;
-    c0 = (c0 - c1) & MASK_VAL;
-    c0 = rol(c0, ALPHA());
-    return(c0, c1);
-
-def expand_key(k, t):
-    ks = [0 for i in range(t)];
-    ks[0] = k[len(k)-1];
-    l = list(reversed(k[:len(k)-1]));
-    for i in range(t-1):
-        l[i%3], ks[i+1] = enc_one_round((l[i%3], ks[i]), i);
-    return(ks);# Essential Definitions
-
-def WORD_SIZE():
-    return(16);
-
-def ALPHA():
-    return(7);
-
-def BETA():
-    return(2);
-
-MASK_VAL = 2 ** WORD_SIZE() - 1;
-
-def shuffle_together(l):
-    state = np.random.get_state();
-    for x in l:
-        np.random.set_state(state);
-        np.random.shuffle(x);
-
-def rol(x,k):
-    return(((x << k) & MASK_VAL) | (x >> (WORD_SIZE() - k)));
-
-def ror(x,k):
-    return((x >> k) | ((x << (WORD_SIZE() - k)) & MASK_VAL));
-
-def enc_one_round(p, k):
-    c0, c1 = p[0], p[1];
-    c0 = ror(c0, ALPHA());
-    c0 = (c0 + c1) & MASK_VAL;
-    c0 = c0 ^ k;
-    c1 = rol(c1, BETA());
-    c1 = c1 ^ c0;
-    return(c0,c1);
-
-def dec_one_round(c,k):
-    c0, c1 = c[0], c[1];
-    c1 = c1 ^ c0;
-    c1 = ror(c1, BETA());
-    c0 = c0 ^ k;
-    c0 = (c0 - c1) & MASK_VAL;
-    c0 = rol(c0, ALPHA());
-    return(c0, c1);
-
-def expand_key(k, t):
-    ks = [0 for i in range(t)];
-    ks[0] = k[len(k)-1];
-    l = list(reversed(k[:len(k)-1]));
-    for i in range(t-1):
-        l[i%3], ks[i+1] = enc_one_round((l[i%3], ks[i]), i);
-    return(ks);# Essential Definitions
-
-def WORD_SIZE():
-    return(16);
-
-def ALPHA():
-    return(7);
-
-def BETA():
-    return(2);
-
-MASK_VAL = 2 ** WORD_SIZE() - 1;
-
-def shuffle_together(l):
-    state = np.random.get_state();
-    for x in l:
-        np.random.set_state(state);
-        np.random.shuffle(x);
-
-def rol(x,k):
-    return(((x << k) & MASK_VAL) | (x >> (WORD_SIZE() - k)));
-
-def ror(x,k):
-    return((x >> k) | ((x << (WORD_SIZE() - k)) & MASK_VAL));
-
-def enc_one_round(p, k):
-    c0, c1 = p[0], p[1];
-    c0 = ror(c0, ALPHA());
-    c0 = (c0 + c1) & MASK_VAL;
-    c0 = c0 ^ k;
-    c1 = rol(c1, BETA());
-    c1 = c1 ^ c0;
-    return(c0,c1);
-
-def dec_one_round(c,k):
-    c0, c1 = c[0], c[1];
-    c1 = c1 ^ c0;
-    c1 = ror(c1, BETA());
-    c0 = c0 ^ k;
-    c0 = (c0 - c1) & MASK_VAL;
-    c0 = rol(c0, ALPHA());
-    return(c0, c1);
-
-def expand_key(k, t):
-    ks = [0 for i in range(t)];
-    ks[0] = k[len(k)-1];
-    l = list(reversed(k[:len(k)-1]));
-    for i in range(t-1):
-        l[i%3], ks[i+1] = enc_one_round((l[i%3], ks[i]), i);
-    return(ks);# Essential Definitions
-
-def WORD_SIZE():
-    return(16);
-
-def ALPHA():
-    return(7);
-
-def BETA():
-    return(2);
-
-MASK_VAL = 2 ** WORD_SIZE() - 1;
-
-def shuffle_together(l):
-    state = np.random.get_state();
-    for x in l:
-        np.random.set_state(state);
-        np.random.shuffle(x);
-
-def rol(x,k):
-    return(((x << k) & MASK_VAL) | (x >> (WORD_SIZE() - k)));
-
-def ror(x,k):
-    return((x >> k) | ((x << (WORD_SIZE() - k)) & MASK_VAL));
-
-def enc_one_round(p, k):
-    c0, c1 = p[0], p[1];
-    c0 = ror(c0, ALPHA());
-    c0 = (c0 + c1) & MASK_VAL;
-    c0 = c0 ^ k;
-    c1 = rol(c1, BETA());
-    c1 = c1 ^ c0;
-    return(c0,c1);
-
-def dec_one_round(c,k):
-    c0, c1 = c[0], c[1];
-    c1 = c1 ^ c0;
-    c1 = ror(c1, BETA());
-    c0 = c0 ^ k;
-    c0 = (c0 - c1) & MASK_VAL;
-    c0 = rol(c0, ALPHA());
-    return(c0, c1);
-
-def expand_key(k, t):
-    ks = [0 for i in range(t)];
-    ks[0] = k[len(k)-1];
-    l = list(reversed(k[:len(k)-1]));
-    for i in range(t-1):
-        l[i%3], ks[i+1] = enc_one_round((l[i%3], ks[i]), i);
-    return(ks);# Essential Definitions
-
-def WORD_SIZE():
-    return(16);
-
-def ALPHA():
-    return(7);
-
-def BETA():
-    return(2);
-
-MASK_VAL = 2 ** WORD_SIZE() - 1;
-
-def shuffle_together(l):
-    state = np.random.get_state();
-    for x in l:
-        np.random.set_state(state);
-        np.random.shuffle(x);
-
-def rol(x,k):
-    return(((x << k) & MASK_VAL) | (x >> (WORD_SIZE() - k)));
-
-def ror(x,k):
-    return((x >> k) | ((x << (WORD_SIZE() - k)) & MASK_VAL));
-
-def enc_one_round(p, k):
-    c0, c1 = p[0], p[1];
-    c0 = ror(c0, ALPHA());
-    c0 = (c0 + c1) & MASK_VAL;
-    c0 = c0 ^ k;
-    c1 = rol(c1, BETA());
-    c1 = c1 ^ c0;
-    return(c0,c1);
-
-def dec_one_round(c,k):
-    c0, c1 = c[0], c[1];
-    c1 = c1 ^ c0;
-    c1 = ror(c1, BETA());
-    c0 = c0 ^ k;
-    c0 = (c0 - c1) & MASK_VAL;
-    c0 = rol(c0, ALPHA());
-    return(c0, c1);
-
-def expand_key(k, t):
-    ks = [0 for i in range(t)];
-    ks[0] = k[len(k)-1];
-    l = list(reversed(k[:len(k)-1]));
-    for i in range(t-1):
-        l[i%3], ks[i+1] = enc_one_round((l[i%3], ks[i]), i);
-    return(ks);# Essential Definitions
-
-def WORD_SIZE():
-    return(16);
-
-def ALPHA():
-    return(7);
-
-def BETA():
-    return(2);
-
-MASK_VAL = 2 ** WORD_SIZE() - 1;
-
-def shuffle_together(l):
-    state = np.random.get_state();
-    for x in l:
-        np.random.set_state(state);
-        np.random.shuffle(x);
-
-def rol(x,k):
-    return(((x << k) & MASK_VAL) | (x >> (WORD_SIZE() - k)));
-
-def ror(x,k):
-    return((x >> k) | ((x << (WORD_SIZE() - k)) & MASK_VAL));
-
-def enc_one_round(p, k):
-    c0, c1 = p[0], p[1];
-    c0 = ror(c0, ALPHA());
-    c0 = (c0 + c1) & MASK_VAL;
-    c0 = c0 ^ k;
-    c1 = rol(c1, BETA());
-    c1 = c1 ^ c0;
-    return(c0,c1);
-
-def dec_one_round(c,k):
-    c0, c1 = c[0], c[1];
-    c1 = c1 ^ c0;
-    c1 = ror(c1, BETA());
-    c0 = c0 ^ k;
-    c0 = (c0 - c1) & MASK_VAL;
-    c0 = rol(c0, ALPHA());
-    return(c0, c1);
-
-def expand_key(k, t):
-    ks = [0 for i in range(t)];
-    ks[0] = k[len(k)-1];
-    l = list(reversed(k[:len(k)-1]));
-    for i in range(t-1):
-        l[i%3], ks[i+1] = enc_one_round((l[i%3], ks[i]), i);
-    return(ks);# Essential Definitions
-
-def WORD_SIZE():
-    return(16);
-
-def ALPHA():
-    return(7);
-
-def BETA():
-    return(2);
-
-MASK_VAL = 2 ** WORD_SIZE() - 1;
-
-def shuffle_together(l):
-    state = np.random.get_state();
-    for x in l:
-        np.random.set_state(state);
-        np.random.shuffle(x);
-
-def rol(x,k):
-    return(((x << k) & MASK_VAL) | (x >> (WORD_SIZE() - k)));
-
-def ror(x,k):
-    return((x >> k) | ((x << (WORD_SIZE() - k)) & MASK_VAL));
-
-def enc_one_round(p, k):
-    c0, c1 = p[0], p[1];
-    c0 = ror(c0, ALPHA());
-    c0 = (c0 + c1) & MASK_VAL;
-    c0 = c0 ^ k;
-    c1 = rol(c1, BETA());
-    c1 = c1 ^ c0;
-    return(c0,c1);
-
-def dec_one_round(c,k):
-    c0, c1 = c[0], c[1];
-    c1 = c1 ^ c0;
-    c1 = ror(c1, BETA());
-    c0 = c0 ^ k;
-    c0 = (c0 - c1) & MASK_VAL;
-    c0 = rol(c0, ALPHA());
-    return(c0, c1);
-
-def expand_key(k, t):
-    ks = [0 for i in range(t)];
-    ks[0] = k[len(k)-1];
-    l = list(reversed(k[:len(k)-1]));
-    for i in range(t-1):
-        l[i%3], ks[i+1] = enc_one_round((l[i%3], ks[i]), i);
-    return(ks);
-    
+  
 def encrypt(p, ks):
     x, y = p[0], p[1];
     for k in ks:
@@ -572,7 +132,7 @@ def make_checkpoint(datei):
     return(res);
 
  
-bs = 5000;
+bs = 50; # Batch Size
 
 def cyclic_lr(num_epochs, high_lr, low_lr):
     res = lambda i: low_lr + ((num_epochs-1) - i % num_epochs)/(num_epochs-1) * (high_lr - low_lr);
